bcdf8092ff38257fba356b30368076117fb5b499
karlminator <kf.holmsten@gmail.com>
2026-07-13 17:15:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UtF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Calendar Project 🗓️✍️</title>
+
+    <meta name="description" content="Calendar, MySQL-PHP-JS" />
+
+    <link rel="stylesheet" href="style.css" />
+    <script src="calendar.js" defer></script>
+  </head>
+
+  <body>
+    <header>
+      <h1>Calendar App</h1>
+    </header>
+    
+    <section class="clock-container">
+      <div id="clock"></div>
+    </section>
+
+    <section class="calender">
+      <div class="nav-btn-container">
+        <button class="nav-btn">◀️</button>
+        <h2 id="monthYear"></h2>
+        <button class="nav-btn">▶️</button>
+      </div>
+
+      <div class="calendar-grid" id="calendar"></div>
+    </section>
+
+    <!-- Modal: Add/Edit/Delete event -->
+     <section id="eventSelectorWrapper">
+      <label for="eventSelector">
+        <strong>Select Event:</strong>
+      </label>
+      <select name="" id="eventSelector">
+        <option disabled selected>Choose Event...</option>
+      </select>
+     </section>
+
+     <!-- Main Form -->
+      <form method="post" id="eventForm">
+        <input type="hidden" name="action" id="formAction" value="add">
+        <input type="hidden" name="event_id" id="eventId">
+
+        <label for="courseName">Course Title:</label>
+        <input type="text" name="course_name" id="courseName" required>
+
+        <label for="instructorName">Instructor Name:</label>
+        <input type="text" name="instructor_name" id="instructorName" required>
+
+        <label for="startDate">Start Date:</label>
+        <input type="date" name="start_date" id="startDate" required>
+
+        <label for="endDate">End Date:</label>
+        <input type="date" name="end_date" id="endDate" required>
+
+        <button type="submit">Save 💾</button>
+      </form>
+
+      <!-- Delete Form -->
+       <form method="post" onsubmit="return confirm('Do you really want to delete this event?')">
+        <input type="hidden" name="action" value="delete">
+        <input type="hidden" name="event_id" id="deleteEventId">
+        <button type="submit" class="submit-btn">Delete 🗑️</button>
+       </form>
+
+       <!-- Close Modal Button -->
+        <button type="button" class="submit-btn">Cancel ❌</button>
+
+  </body>
+
+</html>
